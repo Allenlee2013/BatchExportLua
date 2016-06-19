d3685b5d7a80c6e73d896072e9e738d16f009c8e
allenlee <allenlee2013@gmail.com>
2016-06-19 14:10:05 +0800

    modify batch_export.py, support json modify time key

--- batch_export.py
+++ batch_export.py
@@ -1,162 +1,278 @@
 #!/usr/bin/env python
-# encoding=utf-8
+# -*- encoding=utf-8 -*-
+# batch export lua files
 
 import re
 import os
 import sys
+import pickle
+import commands
 import datetime
 import threading
-import commands
-import pickle
 
 from os.path import join
+from datetime import datetime
 
 class BatchExportLua:
     def __init__(self, rootdir):
         if not os.path.exists(rootdir):
-            print >> sys.stderr, 'invalid directory path: {}'.format(rootdir)
+            print >> sys.stderr, 'invalid path: {}'.format(rootdir)
             sys.exit(1)
 
         os.chdir(rootdir)
-        self.pardir = os.path.abspath(os.pardir)
+        self.__pardir = os.path.abspath(os.pardir)
 
-        self.excels_mtime_dict_file = '_excels_mtime_dict_file'
+        self.__mtime_dict_file = '_mtime_dict_file'
 
-        # create an new file first
-        if not os.path.exists(self.excels_mtime_dict_file):
-            print 'not exists {}'.format(self.excels_mtime_dict_file)
-            open(self.excels_mtime_dict_file, 'wb').close()
-            self.excels_mtime_dict = {}
+        # load the mtime_dict from file
+        if not os.path.exists(self.__mtime_dict_file) \
+                or os.path.getsize(self.__mtime_dict_file) == 0:
+            self.__mtime_dict = {}
         else:
-            # load the mtime_dict from file
-            dict_file = open(self.excels_mtime_dict_file, 'rb')
-            self.excels_mtime_dict = pickle.load(dict_file)
+            dict_file = open(self.__mtime_dict_file, 'rb')
+            self.__mtime_dict = pickle.load(dict_file)
             dict_file.close()
 
         # compile regrex patterns
-        self.split_args_re = re.compile(r'\s*python\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s*')
-        self.extract_filename_withbasedir_re = re.compile(r'(?:\.\./)+([^\s]+)')
+        # python ../../../server_tools/export_lua.py \
+        #       ../../../excels/config/guild/guildconf.xls \
+        #       ../../../server_tools/json/guild/guildconf.json \
+        #       ../../../data/config/guild/guild.lua
+        self.__split_args_re = re.compile(r'\s*python\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s*')
+
+        # old version export python tool
+        # python ../../../python/lua_table_generator.py -f ../../../excels/config/buff/buffconf.xls \
+        #        -o ../../../data/config/buff
+        self.__split_old_ver_args_re = re.compile(r'\s*python\s+(\S+)\s+-f\s+(\S+)\s+-o\s+(\S+)\s*')
+
+        # ../../../excels/config/guild/guildconf.xls
+        self.__extract_filename_withbasedir_re = re.compile(r'(?:\.\./)+(\S+)')
 
         # excels set
-        self.new_excels = set()
-        self.skip_excels = set()
-        self.output_excels = set()
+        self.__new_excels = set()
+        self.__skip_excels = set()
 
         # success output lua files set
-        self.success_output_luas = {}
-        self.success_output_luas_mutex = threading.Lock()
+        self.__success_output_luas = {}
+        self.__success_output_luas_mutex = threading.Lock()
 
         # failure output lua files set
-        self.failure_output_luas = {}
-        self.failure_output_luas_mutex = threading.Lock()
+        self.__failure_output_luas = {}
+        self.__failure_output_luas_mutex = threading.Lock()
+
+        # time
+        self.__starttime = None
+        self.__endtime = None
 
         # threads
-        self.threads = []
+        self.__threads = []
 
-    def export(self, python_file, excel_name, json_file, lua_file):
-        cmd = 'python ' + python_file + ' ' + excel_name + ' ' + json_file + ' ' + lua_file
+    def __export(self, python_file, excel_file, json_file, lua_file):
+        cmd = 'python ' + python_file + ' ' + excel_file + ' ' + json_file + ' ' + lua_file
 
         status, result = commands.getstatusoutput(cmd)
 
-        lua_file_relative_path = lua_file[lua_file.find('data/config'):]
+        lua_file_relative_path = lua_file[lua_file.rfind('../') + 3:]
 
         if status == 0:
-            self.success_output_luas_mutex.acquire()
-            self.success_output_luas[lua_file_relative_path] = result
-            self.success_output_luas_mutex.release()
+            self.__success_output_luas_mutex.acquire()
+            self.__success_output_luas[lua_file_relative_path] = result
+            self.__success_output_luas_mutex.release()
         else:
-            self.failure_output_luas_mutex.acquire()
-            self.failure_output_luas[lua_file_relative_path] = result
-            self.failure_output_luas_mutex.release()
+            self.__failure_output_luas_mutex.acquire()
+            self.__failure_output_luas[lua_file_relative_path] = result
+            self.__failure_output_luas_mutex.release()
 
-    def scan_file(self, filename):
+    def __scan_file(self, filename):
         ext_name = os.path.splitext(filename)[1]
-        if not ext_name in ['.bat']:
+        if not ext_name in ['.bat', '.bat1']:
             return
 
         fi = open(filename, 'r')
 
         for line in fi.readlines():
-            args = self.split_args_re.findall(line)
-            args = len(args) > 0 and args[0] or ''
-            if len(args) == 4:
-                excel_name = join(self.pardir, self.extract_filename_withbasedir_re.findall(args[1])[0])
-
-                excel_mtime = os.stat(excel_name).st_mtime
-
-                skip = False
-                if excel_name in self.new_excels:
-                    pass
-                elif self.excels_mtime_dict.has_key(excel_name):
-                    if self.excels_mtime_dict[excel_name] >= excel_mtime:
-                        skip = True
-                        self.skip_excels.add(excel_name)
+            args = self.__split_old_ver_args_re.findall(line)
+            args = args[0] if len(args) > 0 else ''
+            if len(args) == 3:
+                new_ver = False
+            else:
+                args = self.__split_args_re.findall(line)
+                args = args[0] if len(args) > 0 else ''
+                if len(args) == 4:
+                    new_ver = True
                 else:
-                    self.new_excels.add(excel_name)
-                    self.excels_mtime_dict[excel_name] = excel_mtime
-
-                if skip:
                     continue
 
-                python_file = join(self.pardir, self.extract_filename_withbasedir_re.findall(args[0])[0])
-                json_file = join(self.pardir, self.extract_filename_withbasedir_re.findall(args[2])[0])
-                lua_file = join('..', self.extract_filename_withbasedir_re.findall(args[3])[0])
+            excel_file = join(self.__pardir, self.__extract_filename_withbasedir_re.findall(args[1])[0])
+            excel_mtime = os.stat(excel_file).st_mtime
 
-                # start an new thread 
-                th = threading.Thread(target=self.export, args=(python_file, excel_name, json_file, lua_file))
-                th.start()
-                self.threads.append(th)
+            if new_ver:
+                json_file = join(self.__pardir, self.__extract_filename_withbasedir_re.findall(args[2])[0])
+                json_mtime = os.stat(json_file).st_mtime
+
+            skip = False
+            if excel_file in self.__new_excels:                         # new ?
+                if new_ver:
+                    self.__mtime_dict[json_file] = json_mtime
+
+            elif self.__mtime_dict.has_key(excel_file):
+                if self.__mtime_dict[excel_file] >= excel_mtime: # old ?
+                    if new_ver: # old ?
+                        dict_json_mtime = self.__mtime_dict.get(json_file) 
+                        if json_mtime is not None and dict_json_mtime >= json_mtime:
+                            skip = True
+                            self.__skip_excels.add(excel_file)
+                        else:
+                            self.__mtime_dict[json_file] = json_mtime
+                    else:
+                        skip = True
+                        self.__skip_excels.add(excel_file)
+                else:                                                   # newer ?
+                    self.__new_excels.add(excel_file)
+                    self.__mtime_dict[excel_file] = excel_mtime
+
+                    if new_ver:
+                        self.__mtime_dict[json_file] = json_mtime
+            else:
+                self.__new_excels.add(excel_file)                       # add new excel
+                self.__mtime_dict[excel_file] = excel_mtime
+
+                if new_ver:
+                    self.__mtime_dict[json_file] = json_mtime
+
+            if skip:
+                continue
+
+            python_file = join(self.__pardir, self.__extract_filename_withbasedir_re.findall(args[0])[0])
+
+            if new_ver:
+                #  json_file = join(self.__pardir, self.__extract_filename_withbasedir_re.findall(args[2])[0])
+                lua_file = join('..', self.__extract_filename_withbasedir_re.findall(args[3])[0])
+            else:
+                excel_file = '-f ' + excel_file
+                json_file = '-o'
+                lua_file = join('..', self.__extract_filename_withbasedir_re.findall(args[2])[0])
+
+            # start an new thread to export lua_file
+            th = threading.Thread(target=self.__export, args=(python_file, excel_file, json_file, lua_file))
+            th.start()
+            self.__threads.append(th)
 
         fi.close()
 
-    def scan_dir(self, dirname):
+    def __scan_dir(self, dirname):
         old_dir = os.getcwd()
         os.chdir(dirname)
 
         files = os.listdir('.')
         for f in files:
             if os.path.isdir(f):
-                self.scan_dir(f)
+                self.__scan_dir(f)
             else:
-                self.scan_file(f)
+                self.__scan_file(f)
 
         os.chdir(old_dir)
 
-    def start(self):
-        self.scan_dir('./config')
+    def __start(self):
+        self.__starttime = datetime.now()
+        self.__scan_dir('./config')
 
-    def end(self):
+    def __end(self):
         # wait for threads terminate
-        for th in self.threads:
+        for th in self.__threads:
             th.join()
 
-        # print success_output_luas
-        print 'succes output files:'
-        for filename, result in self.success_output_luas.iteritems():
-            print filename, ':\n\t', result, '\n'
-        print '\n'
+       # pickle excels_mtime_dict
+        dict_file = open(self.__mtime_dict_file, 'wb')
+        pickle.dump(self.__mtime_dict, dict_file)
+        dict_file.close()
 
-        # print failure_output_luas
-        print 'failure output files:'
-        for filename, result in self.failure_output_luas.iteritems():
-            print filename, ':\n\t', result, '\n'
-        print '\n'
+        # record end time
+        self.__endtime = datetime.now()
+
+        # output result
+        self.__output_result()
+
+    class __OutputStream:
+        def __init__(self):
+            self.buff = ''
+            self.console = sys.stdout # save the old stdout
+
+        # public external call to write
+        def write(self, output_stream):
+            self.buff += output_stream
+
+        # private internal call to write
+        def __write(self, streamobj):
+            print >> streamobj, self.buff
+
+        def write2console(self):
+            self.__write(self.console)
+
+        def write2file(self, filename):
+            of = open(filename, 'w')
+            self.__write(of)
+            of.close()
+
+        def append2file(self, filename):
+            of = open(filename, 'a')
+            self.__write(of)
+            of.close()
+
+        def flush(self):
+            self.buff = ''
+
+        def reset(self):
+            sys.stdout = self.console
+
+    def __output_result(self):
+        streamobj = self.__OutputStream()
+        sys.stdout = streamobj
 
         # print skip excels
         print 'skip excels:'
-        for filename in self.skip_excels:
+        for filename in self.__skip_excels:
             print filename
-        print '\n'
+        else:
+            print '\n'
 
-        # pickle excels_mtime_dict
-        dict_file = open(self.excels_mtime_dict_file, 'wb')
-        pickle.dump(self.excels_mtime_dict, dict_file)
-        dict_file.close()
+        # print new excels
+        print 'new excels:'
+        for filename in self.__new_excels:
+            print filename
+        else:
+            print '\n'
+
+        # print success_output_luas
+        print 'succes output files:'
+        for filename, result in self.__success_output_luas.iteritems():
+            print filename, ':\n', result, '\n'
+        else:
+            print '\n'
+
+        # print failure_output_luas
+        print 'failure output files:'
+        for filename, result in self.__failure_output_luas.iteritems():
+            print filename, ':\n\t', result, '\n'
+        else:
+            print '\n'
+
+        # print cost time 
+        print 'cost time:'
+        print '  start  {}'.format(self.__starttime)
+        print '  end    {}'.format(self.__endtime)
+        print '  total  {}'.format(self.__endtime - self.__starttime)
+
+        streamobj.write2console()
+        streamobj.write2file('result.txt')
+        streamobj.flush()
+        streamobj.reset()
 
     def run(self):
-        self.start()
-        self.end()
+        self.__start()
+        self.__end()
 
+# main routine
 if __name__ == '__main__':
     argv = sys.argv
     instance = BatchExportLua('.')
